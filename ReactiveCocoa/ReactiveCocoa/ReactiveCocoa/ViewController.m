32ef4ad92b547c805bd80b8f70c6a60f56802597
Liu6520893 <bing2.liu>
2017-02-21 17:59:26 +0800

    ReactiveCocoa 学习-2017-2-21

--- ReactiveCocoa/ReactiveCocoa/ReactiveCocoa/ViewController.m
+++ ReactiveCocoa/ReactiveCocoa/ReactiveCocoa/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <ReactiveObjC/ReactiveObjC.h>
 
 @interface ViewController ()
 
@@ -17,8 +18,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self signal];
+
 }
 
+-(void)signal {
+
+    //1.创建信号
+    RACSignal *signal = [RACSignal createSignal:^RACDisposable * _Nullable(id<RACSubscriber>  _Nonnull subscriber) {
+
+        //3.发送信号
+        [subscriber sendNext:@1];
+
+        return [RACDisposable disposableWithBlock:^{
+            NSLog(@"默认信号发送完毕被取消！");
+        }];
+
+    }];
+
+    //2.订阅信号
+    RACDisposable *disposable = [signal subscribeNext:^(id  _Nullable x) {
+        NSLog(@"接收 --- %@",x);
+    }];
+
+    [disposable dispose];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
